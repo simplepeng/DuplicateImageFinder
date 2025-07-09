0605e398d88144b8f290acd94ee272d7c1918c52
Peng Chen <383559698@qq.com>
2025-07-09 15:20:23 +0800

    copy file name

--- composeApp/src/desktopMain/kotlin/simple/compose/digfinder/result/ResultDialog.kt
+++ composeApp/src/desktopMain/kotlin/simple/compose/digfinder/result/ResultDialog.kt
@@ -6,21 +6,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.SnackbarHost
 import androidx.compose.material.Text
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
@@ -28,12 +22,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ClipEntry
+import androidx.compose.ui.platform.ClipMetadata
+import androidx.compose.ui.platform.LocalClipboard
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import coil3.compose.AsyncImage
 import kotlinx.coroutines.launch
 import simple.compose.digfinder.data.DuplicateFile
-import simple.compose.digfinder.ext.debugBackground
 import simple.compose.digfinder.ext.formatStr
 
 @Composable
@@ -55,6 +53,8 @@ fun ResultScreenContent(
     val totalSize = remember { duplicateFiles.sumOf { it.file2.size } }
     val coroutineScope = rememberCoroutineScope()
     val snackBarHostState = remember { SnackbarHostState() }
+//    val clipboard = LocalClipboard.current
+    val clipboardManager = LocalClipboardManager.current
 
     Card(
         modifier = Modifier.padding(vertical = 10.dp),
@@ -80,6 +80,8 @@ fun ResultScreenContent(
                     items(duplicateFiles) { item ->
                         RowItem(item, onItemClick = { file ->
                             coroutineScope.launch {
+                                clipboardManager.setText(AnnotatedString(file.name))
+//                                clipboard.setClipEntry(ClipEntry(file.name))
                                 snackBarHostState.showSnackbar("${file.name} copied")
                             }
                         })
