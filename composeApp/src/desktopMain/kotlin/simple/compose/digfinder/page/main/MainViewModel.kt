28ed8fca9870b65f2c46ffd2f2e01be05202ee13
Peng Chen <383559698@qq.com>
2025-07-11 16:59:59 +0800

    update base

--- composeApp/src/desktopMain/kotlin/simple/compose/digfinder/page/main/MainViewModel.kt
+++ composeApp/src/desktopMain/kotlin/simple/compose/digfinder/page/main/MainViewModel.kt
@@ -1,11 +1,5 @@
 package simple.compose.digfinder.page.main
 
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 import simple.compose.digfinder.base.BaseViewModel
 
 class MainViewModel : BaseViewModel<MainAction, MainUIState, MainIntent>(MainUIState.Content) {
