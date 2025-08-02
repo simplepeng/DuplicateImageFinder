05e3bc12ff8de67caed00ba0c2c23b656dd2f020
Peng Chen <383559698@qq.com>
2025-08-02 17:31:48 +0800

    update string.xml

--- composeApp/src/desktopMain/kotlin/simple/compose/digfinder/page/finder/component/FileExistsDialog.kt
+++ composeApp/src/desktopMain/kotlin/simple/compose/digfinder/page/finder/component/FileExistsDialog.kt
@@ -14,6 +14,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import coil3.compose.AsyncImage
+import duplicateimagefinder.composeapp.generated.resources.Res
+import duplicateimagefinder.composeapp.generated.resources.copy_name
+import duplicateimagefinder.composeapp.generated.resources.file_is_exists
+import org.jetbrains.compose.resources.stringResource
 import simple.compose.digfinder.config.Dimen
 import simple.compose.digfinder.data.DuplicateFile
 import simple.compose.digfinder.widget.AppButton
@@ -21,50 +25,52 @@ import simple.compose.digfinder.widget.DialogCard
 
 @Composable
 fun FileExistsDialog(
-    file: DuplicateFile.File,
-    onDismissRequest: () -> Unit,
+   file: DuplicateFile.File,
+   onDismissRequest: () -> Unit,
 ) {
-    Dialog(
-        onDismissRequest = onDismissRequest,
-        properties = DialogProperties(
-            dismissOnClickOutside = false,
-        )
-    ) {
-        Content(file, onDismissRequest)
-    }
+   Dialog(
+      onDismissRequest = onDismissRequest,
+      properties = DialogProperties(
+         dismissOnClickOutside = false,
+      )
+   ) {
+      Content(file, onDismissRequest)
+   }
 }
 
 @Composable
 private fun Content(
-    file: DuplicateFile.File,
-    onDismissRequest: () -> Unit,
+   file: DuplicateFile.File,
+   onDismissRequest: () -> Unit,
 ) {
-    val clipboardManager = LocalClipboardManager.current
+   val clipboardManager = LocalClipboardManager.current
 
-    DialogCard {
-        Column(
-            modifier = Modifier.fillMaxWidth().padding(Dimen.dialogPadding),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(10.dp)
-        ) {
-            Text(
-                text = "Oops! File is Exists"
-            )
-            AsyncImage(
-                model = file.path,
-                contentDescription = null,
-            )
-            Text(
-                text = file.name
-            )
-            AppButton(
-                onClick = {
-                    clipboardManager.setText(AnnotatedString(file.name.substringBefore(".")))
-                    onDismissRequest.invoke()
-                }
-            ) {
-                Text(text = "Copy Name")
+   DialogCard {
+      Column(
+         modifier = Modifier.fillMaxWidth().padding(Dimen.dialogPadding),
+         horizontalAlignment = Alignment.CenterHorizontally,
+         verticalArrangement = Arrangement.spacedBy(10.dp)
+      ) {
+         Text(
+            text = stringResource(Res.string.file_is_exists)
+         )
+         AsyncImage(
+            model = file.path,
+            contentDescription = null,
+         )
+         Text(
+            text = file.name
+         )
+         AppButton(
+            onClick = {
+               clipboardManager.setText(AnnotatedString(file.name.substringBefore(".")))
+               onDismissRequest.invoke()
             }
-        }
-    }
+         ) {
+            Text(
+               text = stringResource(Res.string.copy_name)
+            )
+         }
+      }
+   }
 }
